a9dce60996ecf9a344880111dd916b3e2ad84473
mysticum <mysticum@outlook.com>
2025-03-22 18:17:27 +0100

    First frontend stroke

---
+++ wmsprototype/wmsprototype/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Product)
+admin.site.register(Inventory)
+admin.site.register(User)
+admin.site.register(Task)
+admin.site.register(DocumentType)
+admin.site.register(Document)
+admin.site.register(Warehouse)
+admin.site.register(Department)
+admin.site.register(Row)
+admin.site.register(Section)
+admin.site.register(Level)
+admin.site.register(Cell)
+
--- wmsprototype/wmsprototype/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import *
-
-__all__ = ["Product", "Inventory", "User", "Task", "DocumentType", "Document", "Warehouse", "Department", "Row", "Section", "Level", "Cell"]
