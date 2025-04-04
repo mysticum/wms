2060421c3b78a6bdd77414818147eaac569edc80
mysticum <mysticum@outlook.com>
2025-04-04 12:25:57 +0200

    minor changes linked to models editing

--- wmsprototype/wmsprototype/admin.py
+++ wmsprototype/wmsprototype/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.register(Address)
 admin.site.register(Product)
 admin.site.register(Inventory)
 admin.site.register(AppUser)
-admin.site.register(Task)
 admin.site.register(DocumentType)
 admin.site.register(Document)
 admin.site.register(Warehouse)
@@ -13,4 +13,8 @@ admin.site.register(Row)
 admin.site.register(Section)
 admin.site.register(Level)
 admin.site.register(Cell)
+admin.site.register(Status)
+admin.site.register(DocumentProduct)
+admin.site.register(DocumentUser)
+admin.site.register(DocumentStatus)
 
